6b1ec105ef7d4b66243f4c7ba48a04b66f63fc44
itmam <itmam@RSD-T440.bfh.ch>
2016-10-21 09:28:12 +0200

    implemented test classes

--- src/test/java/ch/bfh/ti/soed/hs16/srs/yellow/customer/PersonTest.java
+++ src/test/java/ch/bfh/ti/soed/hs16/srs/yellow/customer/PersonTest.java
@@ -1,7 +1,7 @@
 package ch.bfh.ti.soed.hs16.srs.yellow.customer;
 
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import java.util.*;
 
@@ -13,7 +13,31 @@ import org.junit.Test;
 public class PersonTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetPersonID() {
+		Person person1 = new Person();
+		assertNotNull(person1.getPersonID());
 	}
+	
+	@Test
+	public void testPersonName() {
+		Person person1 = new Person();
+		
+		String fName = "Barry";
+		String lName = "Allen";
+		
+		person1.setFirstName(fName);
+		person1.setLastName(lName);
+		
+		assertEquals(fName, person1.getFirstName());
+		assertEquals(lName, person1.getLastName());
+	}
+	
+//	@Test
+//	public void testBirthDate() {
+//		Person person1 = new Person();
+//		
+//		Date birthDate = 
+//		assertEquals(birthDate, person1.getDateOfBirth());
+//	}
+	
 }
