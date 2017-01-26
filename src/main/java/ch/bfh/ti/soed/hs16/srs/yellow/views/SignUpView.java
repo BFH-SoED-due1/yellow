244f2b4105274a6883e5fa114dde0611118e7fea
Eric Dubuis <eric.dubuis@bfh.ch>
2017-01-26 12:13:52 +0100

    Evaluation 4, plus summary.

--- src/main/java/ch/bfh/ti/soed/hs16/srs/yellow/views/SignUpView.java
+++ src/main/java/ch/bfh/ti/soed/hs16/srs/yellow/views/SignUpView.java
@@ -9,11 +9,6 @@
 
 package ch.bfh.ti.soed.hs16.srs.yellow.views;
 
-import ch.bfh.ti.soed.hs16.srs.yellow.controllers.JPAProxyDataAccessor;
-import ch.bfh.ti.soed.hs16.srs.yellow.validation.EMailValidationStrategy;
-import ch.bfh.ti.soed.hs16.srs.yellow.validation.PasswordValidationStrategy;
-import ch.bfh.ti.soed.hs16.srs.yellow.validation.UserNameValidationStrategy;
-import ch.bfh.ti.soed.hs16.srs.yellow.validation.ValidationContext;
 import com.vaadin.annotations.Theme;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
@@ -29,115 +24,124 @@ import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 
+import ch.bfh.ti.soed.hs16.srs.yellow.controllers.JPAProxyDataAccessor;
+import ch.bfh.ti.soed.hs16.srs.yellow.validation.EMailValidationStrategy;
+import ch.bfh.ti.soed.hs16.srs.yellow.validation.PasswordValidationStrategy;
+import ch.bfh.ti.soed.hs16.srs.yellow.validation.UserNameValidationStrategy;
+import ch.bfh.ti.soed.hs16.srs.yellow.validation.ValidationContext;
+
 @Theme("mytheme")
 public class SignUpView extends CustomComponent implements View {
 
-    private Panel sPanel = new Panel();
+	// TODO Unused field
+	private Panel sPanel = new Panel();
 
-    private VerticalLayout signVLayout = new VerticalLayout();
+	private VerticalLayout signVLayout = new VerticalLayout();
 
-    private FormLayout signFormLayout = new FormLayout();
+	private FormLayout signFormLayout = new FormLayout();
 
-    private HorizontalLayout sHeader = new HorizontalLayout();
+	private HorizontalLayout sHeader = new HorizontalLayout();
 
-    private TextField sUName = new TextField("Username");
+	private TextField sUName = new TextField("Username");
 
-    private PasswordField sPwd = new PasswordField("Password");
+	private PasswordField sPwd = new PasswordField("Password");
 
-    private TextField fName = new TextField("Firstname");
+	private TextField fName = new TextField("Firstname");
 
-    private TextField lName = new TextField("Lastname");
+	private TextField lName = new TextField("Lastname");
 
-    private TextField emailField = new TextField("Email");
+	private TextField emailField = new TextField("Email");
 
-    private Button backBtn = new Button(" <- Back");
+	private Button backBtn = new Button(" <- Back");
 
-    private Button sSignUpBtn = new Button("Sign up");
+	private Button sSignUpBtn = new Button("Sign up");
 
-    private NavigationRoot navigationRoot;
+	private NavigationRoot navigationRoot;
 
-    private JPAProxyDataAccessor jpaProxyDataAccessor = new JPAProxyDataAccessor();
+	private JPAProxyDataAccessor jpaProxyDataAccessor = new JPAProxyDataAccessor();
 
-    private ValidationContext validationContext = new ValidationContext();
+	private ValidationContext validationContext = new ValidationContext();
 
-    public SignUpView() {
+	public SignUpView() {
 
-        sUName.setRequired(true);
-        sPwd.setRequired(true);
-        fName.setRequired(true);
-        lName.setRequired(true);
-        emailField.setRequired(true);
+		sUName.setRequired(true);
+		sPwd.setRequired(true);
+		fName.setRequired(true);
+		lName.setRequired(true);
+		emailField.setRequired(true);
 
-        sUName.setSizeFull();
-        sPwd.setSizeFull();
-        fName.setSizeFull();
-        lName.setSizeFull();
-        emailField.setSizeFull();
-        sSignUpBtn.setSizeFull();
+		sUName.setSizeFull();
+		sPwd.setSizeFull();
+		fName.setSizeFull();
+		lName.setSizeFull();
+		emailField.setSizeFull();
+		sSignUpBtn.setSizeFull();
 
-        backBtn.addClickListener(e -> {
-            navigationRoot.navigator.navigateTo(NavigationRoot.MAINVIEW);
-        });
+		backBtn.addClickListener(e -> {
+			navigationRoot.navigator.navigateTo(NavigationRoot.MAINVIEW);
+		});
 
-        sSignUpBtn.addClickListener(e -> {
-            String result = checkIfAllFieldsAreValid();
-            if (!(result == "")) {
-                Notification.show(result);
-            } else if (checkIfAllTextEntryNonEmpty()) {
-                jpaProxyDataAccessor.makeCustomer(this.sUName.getValue(), this.sPwd.getValue());
-                Notification.show("User created successfully");
-                backBtn.click();
-            } else {
-                Notification.show("Please fill all required fields to create an user!");
-            }
-        });
+		sSignUpBtn.addClickListener(e -> {
+			String result = checkIfAllFieldsAreValid();
+			if (!(result == "")) {
+				Notification.show(result);
+			} else if (checkIfAllTextEntryNonEmpty()) {
+				jpaProxyDataAccessor.makeCustomer(this.sUName.getValue(), this.sPwd.getValue());
+				Notification.show("User created successfully");
+				backBtn.click();
+			} else {
+				Notification.show("Please fill all required fields to create an user!");
+			}
+		});
 
-        signFormLayout.addComponents(sUName, sPwd, fName, lName, emailField, sSignUpBtn);
-        signFormLayout.setMargin(true);
-        signFormLayout.setWidth(500, Sizeable.Unit.PIXELS);
+		signFormLayout.addComponents(sUName, sPwd, fName, lName, emailField, sSignUpBtn);
+		signFormLayout.setMargin(true);
+		signFormLayout.setWidth(500, Sizeable.Unit.PIXELS);
 
-        sHeader.addComponent(backBtn);
+		sHeader.addComponent(backBtn);
 
-        signVLayout.addComponents(sHeader, signFormLayout);
-        signVLayout.setSpacing(true);
-        signVLayout.setComponentAlignment(signFormLayout, Alignment.MIDDLE_CENTER);
-        signVLayout.setComponentAlignment(sHeader, Alignment.TOP_RIGHT);
+		signVLayout.addComponents(sHeader, signFormLayout);
+		signVLayout.setSpacing(true);
+		signVLayout.setComponentAlignment(signFormLayout, Alignment.MIDDLE_CENTER);
+		signVLayout.setComponentAlignment(sHeader, Alignment.TOP_RIGHT);
 
-        setCompositionRoot(signVLayout);
-    }
+		setCompositionRoot(signVLayout);
+	}
 
-    private boolean checkIfAllTextEntryNonEmpty() {
-        return sUName.getValue() != "" && sPwd.getValue() != "" && fName.getValue() != "" && lName.getValue() != "" && emailField.getValue() != "";
-    }
+	private boolean checkIfAllTextEntryNonEmpty() {
+		return sUName.getValue() != "" && sPwd.getValue() != "" && fName.getValue() != "" && lName.getValue() != ""
+				&& emailField.getValue() != "";
+	}
 
-    private String checkIfAllFieldsAreValid() {
+	private String checkIfAllFieldsAreValid() {
 
-        validationContext.setStrategy(new UserNameValidationStrategy());
-        if (!validationContext.executeStrategy(sUName.getValue())) {
-            return "Sorry, seems like your username contains invalid symbols.\n " +
-                    "Only alphanumeric characters are allowed";
-        }
-        validationContext.setStrategy(new PasswordValidationStrategy());
-        if (!validationContext.executeStrategy(sPwd.getValue())) {
-            return "Sorry, seems like your password is not strong enough. Rules:\n " +
-                    "1) A digit must occur at least once \n " +
-                    "2) A lower and upper case letter must occur at least once \n " +
-                    "3) A special character must occur at least once \n " +
-                    "4) No whitespace allowed in the entire string \n " +
-                    "5) Password must be at least eight places long.";
-        }
-        validationContext.setStrategy(new EMailValidationStrategy());
-        if (!validationContext.executeStrategy(emailField.getValue())) {
-            return "Sorry, seems like you have an invalid e-mail";
-        }
-        return "";
-    }
+		validationContext.setStrategy(new UserNameValidationStrategy());
+		if (!validationContext.executeStrategy(sUName.getValue())) {
+			return "Sorry, seems like your username contains invalid symbols.\n "
+					+ "Only alphanumeric characters are allowed";
+		}
+		validationContext.setStrategy(new PasswordValidationStrategy());
+		if (!validationContext.executeStrategy(sPwd.getValue())) {
+			return "Sorry, seems like your password is not strong enough. Rules:\n "
+					+ "1) A digit must occur at least once \n "
+					+ "2) A lower and upper case letter must occur at least once \n "
+					+ "3) A special character must occur at least once \n "
+					+ "4) No whitespace allowed in the entire string \n "
+					+ "5) Password must be at least eight places long.";
+		}
+		validationContext.setStrategy(new EMailValidationStrategy());
+		if (!validationContext.executeStrategy(emailField.getValue())) {
+			return "Sorry, seems like you have an invalid e-mail";
+		}
+		return "";
+	}
 
-    public void setNavigator(NavigationRoot navigationRoot) {
-        this.navigationRoot = navigationRoot;
-    }
+	public void setNavigator(NavigationRoot navigationRoot) {
+		this.navigationRoot = navigationRoot;
+	}
 
-    public void enter(ViewChangeListener.ViewChangeEvent evt) {
+	@Override
+	public void enter(ViewChangeListener.ViewChangeEvent evt) {
 
-    }
+	}
 }
